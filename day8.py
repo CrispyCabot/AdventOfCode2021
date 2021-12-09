1cd6f0b337ad9cf4faece4753b11ad2d36ed2baf
Chris Bridewell <cjbaseball5@gmail.com>
2021-12-09 14:32:52 -0500

    day8 doesnst work

--- day8.py
+++ day8.py
@@ -10,12 +10,35 @@ def part1():
     print(count)
 
 def part2():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
     possibleMappings = {}
+    for letter in letters:
+        possibleMappings[letter] = ['top', 'topRight', 'topLeft', 'middle', 
+        'bottom', 'bottomRight', 'bottomLeft']
     lines = open('day8.txt', 'r').readlines()
-    for line in lines:
+    inputs = lines[0:1]
+    for line in inputs:
         inp = line.split('|')[0]
         for val in inp.split(' '):
-            if val in possibleMappings:
-                pass
-                
+            if len(val) == 2:
+                for letter in val:
+                    possibleMappings[letter] = getPossibleMappings(possibleMappings[letter], ['topRight', 'bottomRight'])
+            elif len(val) == 3:
+                for letter in val:
+                    possibleMappings[letter] = getPossibleMappings(possibleMappings[letter], ['top', 'topRight', 'bottomRight'])
+            elif len(val) == 4:
+                for letter in val:
+                    possibleMappings[letter] = getPossibleMappings(possibleMappings[letter], ['middle', 'topRight', 'bottomRight', 'topLeft'])
+            elif len(val) == 7:
+                for letter in val:
+                    possibleMappings[letter] = getPossibleMappings(possibleMappings[letter], ['top', 'topRight', 'topLeft', 'middle', 'bottom', 'bottomRight', 'bottomLeft'])
+    print(possibleMappings)
+
+def getPossibleMappings(current, possible):
+    new = []
+    for val in current:
+        if val in possible:
+            new.append(val)
+    return new
+             
 part2()
