c3c67adfe7dc0ddeace2539b690c62a614722cff
Chris Bridewell <cjbaseball5@gmail.com>
2021-12-10 15:40:47 -0500

    day 8 still broken

--- day8.py
+++ day8.py
@@ -10,29 +10,44 @@ def part1():
     print(count)
 
 def part2():
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
-    possibleMappings = {}
-    for letter in letters:
-        possibleMappings[letter] = ['top', 'topRight', 'topLeft', 'middle', 
-        'bottom', 'bottomRight', 'bottomLeft']
+    mappings = {}
     lines = open('day8.txt', 'r').readlines()
     inputs = lines[0:1]
     for line in inputs:
-        inp = line.split('|')[0]
-        for val in inp.split(' '):
-            if len(val) == 2:
-                for letter in val:
-                    possibleMappings[letter] = getPossibleMappings(possibleMappings[letter], ['topRight', 'bottomRight'])
-            elif len(val) == 3:
-                for letter in val:
-                    possibleMappings[letter] = getPossibleMappings(possibleMappings[letter], ['top', 'topRight', 'bottomRight'])
-            elif len(val) == 4:
-                for letter in val:
-                    possibleMappings[letter] = getPossibleMappings(possibleMappings[letter], ['middle', 'topRight', 'bottomRight', 'topLeft'])
-            elif len(val) == 7:
-                for letter in val:
-                    possibleMappings[letter] = getPossibleMappings(possibleMappings[letter], ['top', 'topRight', 'topLeft', 'middle', 'bottom', 'bottomRight', 'bottomLeft'])
-    print(possibleMappings)
+        values = line.split(' | ')[0].split(' ')
+        #find the 1
+        one = ''
+        searching = True
+        while searching:
+            for val in values:
+                if len(val) == 2:
+                    mappings[val] = 1
+                if len(val) == 3:
+                    mappings[val] = 7
+                if len(val) == 4:
+                    mappings[val] = 4
+                if len(val) == 7:
+                    mappings[val] = 8
+                if len(val) == 5:
+                    print("found length 5")
+                    sevenFour = []
+                    for key in mappings:
+                        if mappings[key] == 7 or mappings[key] == 4:
+                            for letter in key:
+                                sevenFour.append(letter)
+                    sevenFour = set(sevenFour)
+                    print(sevenFour)
+                    print(val)
+                    count = 0
+                    for letter in sevenFour:
+                        if letter in val:
+                            count += 1
+                    if count == 4:
+                        mappings[val] = 5
+            if len(mappings) == 5:
+                searching = False
+
+    print(mappings)
 
 def getPossibleMappings(current, possible):
     new = []
